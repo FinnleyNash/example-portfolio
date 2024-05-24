4c70d776a0a444d3615ce61fb3ee61bfaaf18db3
FinnleyNash <finnleynash.nash@gmail.com>
2024-05-24 13:37:39 +0100

    scroll

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,12 +1,22 @@
 import "./HeroImgStyles.css";
 
-import React from 'react'
+import React from 'react';
+import IntroImg from "../assets/intro-bg.jpg"
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
     <div className="hero">
         <div className="mask">
-            
+          <img className="intro-img" src={IntroImg} alt="IntroImg"/>
+        </div>
+        <div className="content">
+          <p>Hello, My name is Finnley</p>
+          <h1>React Developer</h1>
+          <div>
+            <Link to="/project" className="btn">Projects</Link>
+            <Link to="/contact" className="btn-light">Contact</Link>
+          </div>
         </div>
     </div>
   )
