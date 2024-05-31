07be4080c94b56a7a5f51878438beb1eac7a8dc2
FinnleyNash <finnleynash.nash@gmail.com>
2024-05-31 09:36:36 +0100

    form

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,10 +1,33 @@
+import { Link } from "react-router-dom";
 import "./AboutContentStyles.css";
 
 import React from 'react'
 
+import React1 from "../assets/react1.jpg";
+import project4 from "../assets/project4.png";
+
+
 const AboutContent = () => {
   return (
-    <div>AboutContent</div>
+    <div className="about">
+        <div className="left">
+                <h1>who Am I?</h1>
+                <p>I am a React Frontend Developer, i create responsive secure websites.</p>
+                <Link to="/contact">
+                    <button className="btn">Contact</button>
+                </Link>
+        </div>
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack  top">
+                    <img src={React1} className="img" alt="true"/>
+                </div>
+                <div className="img-stack  bottom">
+                    <img src={project4} className="img" alt="true"/>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
